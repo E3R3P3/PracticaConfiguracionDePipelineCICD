0f85703fd2ec1313e4a0a1ecc8f316567ec5063d
E3R3P3 <113480340+E3R3P3@users.noreply.github.com>
2025-04-20 12:39:15 -0400

    Error Unit

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -1,5 +1,5 @@
 // tests/app.test.js
-//const suma = require('../src/app');
+const suma = require('../src/app');
 
 test('suma de 1 + 2 es igual a 3', () => { // ✅ Corregido
   expect(suma(1, 2)).toBe(3);
