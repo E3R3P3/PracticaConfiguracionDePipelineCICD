7f1fa32923d8ca9b5a53791825afceafab94670e
E3R3P3 <113480340+E3R3P3@users.noreply.github.com>
2025-04-20 12:28:01 -0400

    Second commit

--- test/unit.test.js
+++ test/unit.test.js
@@ -1,6 +1,6 @@
 // tests/app.test.js
 const suma = require('../src/app');
 
-test('suma de 1 + 2 es igual a 3', () => {
+test('suma de 1 + 2 es igual a 3', () => { // ✅ Corregido
   expect(suma(1, 2)).toBe(3);
 });
